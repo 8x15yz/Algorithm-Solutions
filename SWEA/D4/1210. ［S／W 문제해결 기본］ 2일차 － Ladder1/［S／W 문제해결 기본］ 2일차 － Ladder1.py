61f14cded7023a0721f9e98608d7b8d7d11ee4b4
김현주 (Jay kim) <8x15yz@gmail.com>
2022-06-01 20:04:42 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 217 ms, Memory: 62,204 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.py
@@ -1,16 +1,18 @@
+# import sys
+# sys.stdin = open('1210ans.txt', 'r')
+
+d = [(-1, 0), (0, -1), (0, 1)]
+
 for tc in range(1, 11):
     T = int(input())
-    arr = [[0]+list(map(int, input().split()))+[0] for _ in range(100)]
+    arr = [list(map(int, input().split())) for _ in range(100)]
 
-    for i in range(102):
-        if arr[99][i] == 2:
-            hori = i
-    ver = 99
-    up = [-1, 0, 0]
-    lr = [0, -1, 1]
-    while ver !=0 :
-        for i in range(3):
-            if arr[ver+up[i]][hori+lr[i]] == 1:
-                arr[ver][hori] = 0
-                ver, hori = ver+up[i], hori+lr[i]
-    print('#{} {}'.format(tc, hori-1))
+    si, sj = 99, arr[99].index(2)
+
+    while si > 0:
+        for di, dj in d:
+            if 0 <= si + di <= 99 and 0 <= sj + dj <= 99 and arr[si + di][sj + dj] == 1:
+                arr[si][sj] = 0
+                si, sj = si+di, sj+dj
+
+    print('#{} {}'.format(tc, sj))
