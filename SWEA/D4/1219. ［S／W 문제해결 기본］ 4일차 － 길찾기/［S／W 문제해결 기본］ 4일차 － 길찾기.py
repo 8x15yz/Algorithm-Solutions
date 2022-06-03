bf9ea7eced0d0b0860f3119be95392cb3f79cf63
김현주 (Jay kim) <8x15yz@gmail.com>
2022-06-03 18:36:49 +0900

    Update ［S／W 문제해결 기본］ 4일차 － 길찾기.py

--- SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.py
+++ SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.py
@@ -1,19 +1,27 @@
-for _ in range(10):
-   tc, V = map(int, input().split())
-   lst = list(map(int, input().split()))
-   arr = {}
-   for i in range(0, V*2, 2):
-       v = lst[i]
-       e= lst[i+1]
-       arr[v] = arr.get(v, [])+ [e]
-   stack = []
-   stack.append(0)
-   visit = [0] * 100
-   while stack:
-       x = stack.pop()
-       visit[x] = 1
-       if x in arr.keys():
-           for w in arr[x]:
-               if not visit[w]:
-                   stack.append(w)
-   print('#{} {}'.format(tc, visit[-1]))
+# import sys
+# sys.stdin = open('1219ans.txt', 'r')
+
+
+for _ in range(1, 11):
+    tc, N = map(int, input().split())
+    datum = list(map(int, input().split()))
+    graph = [[] for _ in range(100)]
+
+    for n in range(N):
+        graph[datum[n*2]].append(datum[n*2+1])
+
+    answerFlag = 0
+    stack = [0]
+    visited = []
+
+    while stack:
+        v = stack.pop()
+        visited.append(v)
+        if v == 99:
+            answerFlag = 1
+            break
+        if graph[v]:
+            for node in graph[v]:
+                stack.append(node)
+
+    print('#{} {}'.format(tc, answerFlag))
