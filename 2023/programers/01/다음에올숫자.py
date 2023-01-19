45fa044b67af99b36573836fba776be9dbd959d6
김현주 <8x15yz@gmail.com>
2023-01-19 17:29:17 +0900

    Create 다음에올숫자.py

---
+++ 2023/programers/01/다음에올숫자.py
@@ -0,0 +1,10 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120924
+# 5분
+
+def solution(common):
+    answer = 0
+    if common[1]-common[0] == common[2]-common[1]:
+        answer = common[-1] + common[1] - common[0]
+    else:
+        answer = common[-1] * (common[1]/common[0])
+    return answer
