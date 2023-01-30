1ab360dd97989b5a2450122ea1e0f09e0ab4d724
김현주 <8x15yz@gmail.com>
2023-01-30 12:30:56 +0900

    Update 실패율.py

--- 2023/programers/01/실패율.py
+++ 2023/programers/01/실패율.py
@@ -1,6 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42889
 
-# 틀린코드 다시 풀어봐야됨
+# 틀린코드 다시 풀어봐야됨 
+# 74.1점
 def solution(N, stages):
     answer = []
     stgUsers = [0 for _ in range(N+1)]
@@ -27,3 +28,28 @@ def solution(N, stages):
                 visited[i] = 1
         
     return answer
+
+[다중리스트 정렬방법](https://shoney.tistory.com/entry/Python-%EB%8B%A4%EC%A4%91-%EB%A6%AC%EC%8A%A4%ED%8A%B8List-%EC%A0%95%EB%A0%AC-%EB%B0%A9%EB%B2%95-%EC%9D%B8%EB%8D%B1%EC%8A%A4-%EC%9A%B0%EC%84%A0%EC%88%9C%EC%9C%84)
+# 다중 리스트 정렬방법으로 풀어봄
+# 오답, 70.4점
+def solution(N, stages):
+    answer = []
+    stage = [0 for _ in range(N+1)] # 0:완료, 1~N:잔류
+    player = len(stages)
+    for mem in stages:
+        if mem == N+1:
+            stage[0] += 1
+        else:
+            stage[mem] += 1
+            
+    failure = [[0, i+1] for i in range(N)]
+    for mem in range(1, len(stage)):
+        failure[mem-1][0] = stage[mem]/player
+        player -= stage[mem]
+        
+    for ans in sorted(failure, reverse=True, key=lambda x:x[0]):
+        answer.append(ans[1])
+    
+    return answer
+
+
