422aaab584b58c738dccc5503576a3daf77666b4
김현주 <8x15yz@gmail.com>
2023-01-31 13:44:44 +0900

    Update 다트게임.py

--- 2023/programers/01/다트게임.py
+++ 2023/programers/01/다트게임.py
@@ -30,8 +30,41 @@ def solution(dartResult):
             round[i] *= 2
         elif option[i] == '#':
             if i != 0:
-                if option[i-1] == '*':
-                    round[i] *= 2
+                if option[i-1] == '*': ## 이부분
+                    round[i] *= 2      ## 지우니까 맞음 => 문제를 잘못 이해했음
+            round[i] *= -1
+
+    answer = sum(round)
+    return answer
+
+def solution(dartResult):
+    answer = 0
+    round , bonus, option = [0, 0, 0], ['', '', ''], ['', '', '']
+    i, digit = -1, ''
+    bonuscnt = {'S' : 1, 'D' : 2, 'T' : 3}
+    # isalpha isdigit isalnum 사용
+    # 알파벳 바로 앞에는 항상 숫자
+    for dt in dartResult:
+        if dt.isdigit():
+            digit += dt
+        elif dt.isalpha():
+            i += 1
+            round[i] = int(digit)
+            digit = ''
+            bonus[i] = dt
+        elif dt.isalnum() == False:
+            option[i] = dt
+
+    for i in range(3):
+        # 보너스 계산
+        round[i] = round[i]**bonuscnt[bonus[i]]
+        
+        # 옵션 계산
+        if option[i] == '*':
+            if i != 0:
+                round[i-1] *= 2
+            round[i] *= 2
+        elif option[i] == '#':
             round[i] *= -1
 
     answer = sum(round)
