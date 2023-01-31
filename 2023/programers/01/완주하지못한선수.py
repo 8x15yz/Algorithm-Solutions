125e67c13f241b32415f3284a63f9652c0baaa9f
김현주 <8x15yz@gmail.com>
2023-01-31 10:22:00 +0900

    Update 완주하지못한선수.py

--- 2023/programers/01/완주하지못한선수.py
+++ 2023/programers/01/완주하지못한선수.py
@@ -1,5 +1,6 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42576
 
+# 틀린코드
 def solution(participant, completion):
     answer = ''
     cnt = [0 for _ in range(len(completion))]
@@ -11,3 +12,25 @@ def solution(participant, completion):
     else:
         answer = completion[cnt.index(2)]
     return answer
+
+
+# 해시함수로 구현했는데 틀림
+def solution(participant, completion):
+    answer = ''
+    part = {}
+    for c in completion:
+        part[c] = 0
+    for pt in participant:
+        if pt in part:
+            part[pt] += 1
+        else:
+            answer = pt
+            break
+    else:
+        for pt in part:
+            if part[pt] == 2:
+                answer = pt
+                break
+    return answer
+
+# 정답코드
