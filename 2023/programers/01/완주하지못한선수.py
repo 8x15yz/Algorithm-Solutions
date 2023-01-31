b4e86825492bf0aa12ae14f7665c2f420edf61a4
김현주 <8x15yz@gmail.com>
2023-01-31 10:37:38 +0900

    Update 완주하지못한선수.py

--- 2023/programers/01/완주하지못한선수.py
+++ 2023/programers/01/완주하지못한선수.py
@@ -34,3 +34,29 @@ def solution(participant, completion):
     return answer
 
 # 정답코드
+def solution(participant, completion):
+    answer = ''
+    part = {}
+    for c in completion:
+        if c in part:
+            part[c][1] += 1
+        else:
+            part[c] = [0, 1]
+        
+    for pt in participant:
+        if pt in part:
+            part[pt][0] += 1
+        else:
+            answer = pt
+            break
+    else:
+        for pt in part:
+            if part[pt][1]/part[pt][0] < 1:
+                answer = pt
+            
+    return answer
+
+# 설명:
+# 분자 = 완료한사람 수
+# 분모 = 참가한사람 수
+# 분수로 나타내서 1보다 작으면 동명이인 모두 못 들어왔다는 얘기라서 그 이름을 answer 로 
