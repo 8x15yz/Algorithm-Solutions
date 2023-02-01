475afea9f469e7754761c89983567f6226ad5601
김현주 <8x15yz@gmail.com>
2023-02-01 09:43:21 +0900

    Update 예산.py

--- 2023/programers/02/예산.py
+++ 2023/programers/02/예산.py
@@ -1,1 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12982
+def solution(d, budget):
+    answer = 0
+    # 그리디
+    # 최대 몇개의 부서에 지원이면 작은 애부터 
+    for i in sorted(d):
+        if budget < i:
+            break
+        else:
+            budget -= i
+            answer += 1
+        
+    return answer
