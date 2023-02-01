1533068df82525ef516303ed7ed4719d202549c5
김현주 <8x15yz@gmail.com>
2023-02-01 10:11:13 +0900

    Update 직사각형별찍기.py

--- 2023/programers/02/직사각형별찍기.py
+++ 2023/programers/02/직사각형별찍기.py
@@ -1,1 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12969
+# 문제도 아님
+a, b = map(int, input().strip().split(' '))
+for i in range(b):
+    for j in range(a):
+        print('*', end='')
+    print()
