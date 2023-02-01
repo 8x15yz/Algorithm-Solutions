66dc4ada15ea147ce4f3261528677c341f4a6784
김현주 <8x15yz@gmail.com>
2023-02-01 10:06:33 +0900

    Update 소수만들기.py

--- 2023/programers/02/소수만들기.py
+++ 2023/programers/02/소수만들기.py
@@ -1,1 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12977
+# 이터툴즈 짱
+from itertools import combinations
+
+def solution(nums):
+    answer = 0
+    for comset in list(combinations(nums, 3)):
+        for s in range(2, sum(comset)):
+            if sum(comset) % s == 0:
+                break
+        else:
+            answer += 1
+                
+    return answer
