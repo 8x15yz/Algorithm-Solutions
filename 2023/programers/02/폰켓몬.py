375105c5c2aae52cd49bea99dda3cdff40465256
김현주 <8x15yz@gmail.com>
2023-02-01 11:10:51 +0900

    Update 폰켓몬.py

--- 2023/programers/02/폰켓몬.py
+++ 2023/programers/02/폰켓몬.py
@@ -1,1 +1,26 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/1845
+# 조합으로 했더니 시간초과 ..
+from itertools import combinations
+def solution(nums):
+    answer = 0
+    N = len(nums)
+    N2 = int(len(nums)/2)
+    ans = []
+    for pset in list(combinations(nums, N2)):
+        ans.append(len(set(list(pset))))
+    answer = max(ans)
+    return answer
+  
+# 이것도 시간초과 ..
+from itertools import combinations
+def solution(nums):
+    answer = 0
+    n = len(nums)//2
+    for pset in list(combinations(nums, n)):
+        if len(set(list(pset))) == n:
+            answer = n
+            break
+        else:
+            if answer < len(set(list(pset))):
+                answer = len(set(list(pset)))
+    return answer
