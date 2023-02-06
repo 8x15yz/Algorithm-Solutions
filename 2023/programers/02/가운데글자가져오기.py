0768350619171a1a95b62d730e8afea0ae5103a6
김현주 <8x15yz@gmail.com>
2023-02-06 11:11:56 +0900

    Create 가운데글자가져오기.py

---
+++ 2023/programers/02/가운데글자가져오기.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12903
+def solution(s):
+    answer = ''
+    if len(s)%2 == 0:
+        answer = s[len(s)//2-1:len(s)//2+1]
+    else:
+        answer = s[len(s)//2]
+    return answer
