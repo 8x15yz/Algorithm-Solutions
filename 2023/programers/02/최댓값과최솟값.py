a1aa7b4f53d2c71c451ac7f77e4c9ee09f577774
김현주 <8x15yz@gmail.com>
2023-02-07 12:30:12 +0900

    Update 최댓값과최솟값.py

--- 2023/programers/02/최댓값과최솟값.py
+++ 2023/programers/02/최댓값과최솟값.py
@@ -17,3 +17,23 @@ def solution(s):
             
     answer = str(min(news)) + ' ' + str(max(news))
     return answer
+
+
+#  당연함 바보라서 그럼
+def solution(s):
+    answer = ''
+    s = list(s+' ')
+    news = []
+    flag = 0
+    wc = 0
+    while wc != len(s):
+        if s[wc] == ' ':
+            if s[flag:wc][0] == '-':
+                news.append(-1*int(''.join(s[flag+1:wc])))
+            else:
+                news.append(int(''.join(s[flag:wc])))
+            flag = wc+1
+        wc += 1
+            
+    answer = str(min(news)) + ' ' + str(max(news))
+    return answer
