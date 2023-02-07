adfd26c59b778b7ffdf9a4948bcbba03b066bc05
김현주 <8x15yz@gmail.com>
2023-02-07 12:20:06 +0900

    Create 최댓값과최솟값.py

---
+++ 2023/programers/02/최댓값과최솟값.py
@@ -0,0 +1,19 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12939
+# 테케는 다 맞는데 왜 채점은 0점임'
+def solution(s):
+    answer = ''
+    s = list(s+' ')
+    news = []
+    flag = 0
+    wc = 0
+    while wc != len(s):
+        if s[wc] == ' ':
+            if s[flag:wc][0] == '-':
+                news.append(-1*int(s[flag:wc][1]))
+            else:
+                news.append(int(s[flag:wc][0]))
+            flag = wc+1
+        wc += 1
+            
+    answer = str(min(news)) + ' ' + str(max(news))
+    return answer
