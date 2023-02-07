12a0544307a260f133d3f8f49f5e4667320b7e27
김현주 <8x15yz@gmail.com>
2023-02-07 10:44:57 +0900

    Update 소수찾기.py

--- 2023/programers/02/소수찾기.py
+++ 2023/programers/02/소수찾기.py
@@ -1,6 +1,7 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/12921
 
 # 시간초과 오답
+# 깡으로 브루트포스 돌린 코드
 def solution(n):
     answer = 0
     for i in range(2, n+1):
@@ -14,4 +15,14 @@ def solution(n):
     return answer
   
   
-# 정답코드
+# 시간초과 오답 2
+# 소수 배열에 대해 나눠보고 나눠지지 않으면 소수 배열에 수를 추가하는 방식
+def solution(n):
+    thtn = []
+    for i in range(2, n+1):
+        for t in thtn:
+            if i%t == 0:
+                break
+        else:
+             thtn.append(i)  
+    return len(thtn)
