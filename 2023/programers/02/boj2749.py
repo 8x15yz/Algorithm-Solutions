f8fee7228f337bbef5d2183c5e618103a5754747
김현주 <8x15yz@gmail.com>
2023-02-08 15:30:36 +0900

    Update boj2749.py

--- 2023/programers/02/boj2749.py
+++ 2023/programers/02/boj2749.py
@@ -57,3 +57,39 @@ print(sum(fmans[0])%1000000)
 
 #일단 행렬곱을 반복문으로만 돌린 버전 => 시간초과 났음
 
+
+
+# 행렬곱연산을 재귀로 만들어서 통과
+def power(baseMtrx, matrix):
+    rstMtrx = [[0, 0], [0, 0]]
+    rstMtrx[0][0] = (matrix[0][0] * baseMtrx[0][0]) + (matrix[0][1] * baseMtrx[1][0])
+    rstMtrx[0][1] = (matrix[0][0] * baseMtrx[0][1]) + (matrix[0][1] * baseMtrx[1][1])
+    rstMtrx[1][0] = (matrix[1][0] * baseMtrx[0][0]) + (matrix[1][1] * baseMtrx[1][0])
+    rstMtrx[1][1] = (matrix[1][0] * baseMtrx[0][1]) + (matrix[1][1] * baseMtrx[1][1])
+    # 65 - 68 코드는 다음과도 같이 작성할 수 있음 
+    #for i in range(2):
+    #    for j in range(2):
+    #        for k in range(2):
+    #            rstMtrx[i][j] += (matrix[i][k] * baseMtrx[k][j])
+    return rstMtrx
+
+
+def multiply(baseMtrx, B):
+    if B == 1:
+        return baseMtrx
+
+    elif B % 2:
+        matrix = multiply(baseMtrx, B - 1)
+        rtnMtrx = power(baseMtrx, matrix)
+        return rtnMtrx
+
+    else:
+        matrix = multiply(baseMtrx, B // 2)
+        rtnMtrx = power(matrix, matrix)
+        return rtnMtrx
+
+
+N = int(input())
+baseMtrx = [[1, 1], [1, 0]]
+result = multiply(baseMtrx, N % 1500000)
+print(result[0][1] % 1000000)
