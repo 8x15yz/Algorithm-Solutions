310fa0997f63922a02c4592a7fffc4bfc099cc9d
김현주 <8x15yz@gmail.com>
2023-02-09 14:07:59 +0900

    Create boj1463.py

---
+++ 2023/programers/02/boj1463.py
@@ -0,0 +1,22 @@
+# dp 문제 
+N = int(input())
+ans = 0
+def calcul(n):
+  global ans
+  if n == 1:
+    return 1
+  elif n%2 == 0:
+    ans += 1
+    return calcul(n//2)
+  elif (n-3)%6 == 0:
+    ans += 1
+    return calcul(n//3)
+  else:
+    ans += 1
+    return calcul(n-1)
+
+calcul(N)
+print(ans-1)
+
+
+#이게 아니래 ..
