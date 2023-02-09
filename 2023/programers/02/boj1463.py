37095ef3f5b7f74161b5d82561eb242e09d99ebf
김현주 <8x15yz@gmail.com>
2023-02-09 15:05:09 +0900

    Update boj1463.py

--- 2023/programers/02/boj1463.py
+++ 2023/programers/02/boj1463.py
@@ -20,3 +20,32 @@ print(ans-1)
 
 
 #이게 아니래 ..
+
+import math
+
+N = int(input())
+ans = 0
+
+
+def calcul(n):
+    global ans
+    if n == 1:
+        return ans
+    elif math.sqrt(n-1) == int(math.sqrt(n-1)):
+        ans += 1
+        return calcul(n - 1)
+    elif n % 2 == 0:
+        ans += 1
+        return calcul(n // 2)
+    elif (n - 1) % 6 == 0:
+        ans += 1
+        return calcul(n - 1)
+    elif (n + 3) % 6 == 0:
+        ans += 1
+        return calcul(n // 3)
+
+
+print(calcul(N))
+
+
+# tq
