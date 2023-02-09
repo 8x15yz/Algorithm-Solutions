e25af8923f2b650c42224a17ff2c301834cc14ec
김현주 <8x15yz@gmail.com>
2023-02-09 15:49:50 +0900

    Update boj1463.py

--- 2023/programers/02/boj1463.py
+++ 2023/programers/02/boj1463.py
@@ -49,3 +49,23 @@ print(calcul(N))
 
 
 # tq
+
+
+# 정답코드
+# 재귀가 아니라 이게 맞다니 정말 분하다
+N = int(input())
+
+dp = [0, 0, 1, 1]
+for i in range(4, N + 1):
+    if i % 2 == 0 and i % 3 == 0:
+        dp.append(min(dp[i // 2] + 1, dp[i // 3] + 1, dp[i - 1] + 1))
+    elif i % 2 == 0 and i % 3 != 0:
+        dp.append(min(dp[i // 2] + 1, dp[i - 1] + 1))
+    elif i % 2 != 0 and i % 3 == 0:
+        dp.append(min(dp[i // 3] + 1, dp[i - 1] + 1))
+    else:
+        dp.append(dp[i - 1] + 1)
+if N == 1:
+    print(0)
+else:
+    print(dp[-1])
