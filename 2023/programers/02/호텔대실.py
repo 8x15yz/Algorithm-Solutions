4766a21f41c9ee8d1ca0bc0350abc38c437254cf
김현주 <8x15yz@gmail.com>
2023-02-13 13:13:13 +0900

    Update 호텔대실.py

--- 2023/programers/02/호텔대실.py
+++ 2023/programers/02/호텔대실.py
@@ -12,16 +12,31 @@ def solution(book_time):
     hotel = [[(0, 0)]]
     for i in range(len(InOuty)):
         for room in range(len(hotel)):
-            print(room)
             if hotel[room][-1][1] <= InOuty[i][0]:
                 hotel[room].append(InOuty[i])
             else:
                 hotel.append([InOuty[i]])
                 #break <= 여기에서 에러남
-        
-        print(hotel[-1])
     return len(hotel)
 
 
 
-# 뜯어고쳐야됨
+# 완전 바보같았던거임
+# 정답코드
+def solution(book_time):
+    InOuty = []
+    
+    for i in range(len(book_time)):
+        InOuty.append((int(book_time[i][0][:2])*60 + int(book_time[i][0][3:])
+                      ,(int(book_time[i][1][:2])*60 + int(book_time[i][1][3:])+10)))
+    InOuty = sorted(InOuty, key=lambda x:x[0])
+    
+    hotel = [[(0, 0)]]
+    for i in range(len(InOuty)):
+        for room in range(len(hotel)):
+            if hotel[room][-1][1] <= InOuty[i][0]:
+                hotel[room].append(InOuty[i])
+                break
+        else:
+            hotel.append([InOuty[i]])
+    return len(hotel)
