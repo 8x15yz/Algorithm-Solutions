e19e444c09d08857396f0b63e1b9e9355adfeea7
김현주 <8x15yz@gmail.com>
2023-02-15 14:58:16 +0900

    Update 15sql.sql

--- 2023/programers/02/15sql.sql
+++ 2023/programers/02/15sql.sql
@@ -27,3 +27,46 @@ SELECT ANIMAL_ID, NAME
 FROM ANIMAL_INS
 WHERE INTAKE_CONDITION not in ("Aged")
 ORDER BY ANIMAL_ID ;
+
+
+# 동물의 아이디와 이름
+# https://school.programmers.co.kr/learn/courses/30/lessons/59403
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+
+# 여러 기준으로 정렬하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/59404
+# 이름은 오름차순으로, 같은 이름이 있으면 날짜는 내림차순으로
+SELECT ANIMAL_ID, NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY NAME ASC, DATETIME DESC
+
+
+# 모든 레코드 조회하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/59034
+SELECT *
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
+
+
+# 역순 정렬하기 => DESC 내림차순
+# https://school.programmers.co.kr/learn/courses/30/lessons/59035
+SELECT NAME, DATETIME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID DESC
+
+
+# 이름이 없는 동물의 아이디
+# https://school.programmers.co.kr/learn/courses/30/lessons/59039
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NULL
+ORDER BY ANIMAL_ID ASC
+
+# 최댓값 구하기
+# https://school.programmers.co.kr/learn/courses/30/lessons/59415
+SELECT MAX(DATETIME)
+FROM ANIMAL_INS
+
