7f3f7e4d5f4bd9da1901218333c5b7a81827042c
Jay Kim <8x15yz@gmail.com>
2023-02-22 17:33:26 +0900

    Update 올바른괄호.py

--- 2023/programers/02/올바른괄호.py
+++ 2023/programers/02/올바른괄호.py
@@ -17,4 +17,25 @@ def solution(s):
         answer = False
     return answer
   
-# 맞은코드
+# 틀리고 있는 코드
+# 이거 뭐임 .. 내가모르는 함정 뭐임 이거 
+# 테케 자꾸 틀림 ..
+def solution(s):
+    s = list(s)
+    answer = True
+    stack = [s.pop(0)]
+    while s:
+        n = s.pop(0)
+        if n == ')':
+            if stack == []:
+                break
+            stack.pop()
+        else:
+            stack.append(n)
+            
+    print(s, stack)
+    if stack == []:
+        answer = True
+    else:
+        answer = False
+    return answer
