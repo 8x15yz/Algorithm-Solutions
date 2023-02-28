409cf6f1cfd62f29e8e18da006a30a1bc77ff1a5
Jay Kim <8x15yz@gmail.com>
2023-02-28 10:42:48 +0900

    Create 이진변환반복하기.py

---
+++ 2023/programers/02/이진변환반복하기.py
@@ -0,0 +1,15 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/70129
+def solution(s):
+    cnt, lev = 0, 0
+    while True:
+        if s == '1':
+            break
+        for i in s:
+            if i == '0':
+                cnt += 1
+        else:
+            # print(str(bin(len(s)-cnt)[2:]))
+            s = str(bin(len(s)-cnt)[2:])
+            lev += 1
+    answer = [lev, cnt]
+    return answer
