98a78df367ff70bfbee6c6ed4e87f902af72cc07
Jay Kim <8x15yz@gmail.com>
2023-03-02 11:03:43 +0900

    Update 숫자의표현.py

--- 2023/programers/03/숫자의표현.py
+++ 2023/programers/03/숫자의표현.py
@@ -4,9 +4,29 @@ def solution(n):
     answer = 0
     for i in range(n):
         su = 0
-        for j in range(i, n):
+        for j in range(i, n+1):
+            su += j
             if su == n:
                 answer += 1
                 break
-            su += j
+            elif su > n:
+                break
+                
     return answer
+
+
+# 비슷한 코드인것같은데 
+def solution(n):
+    count = 1
+    for j in range(1,n):
+        m1 = j
+        while True:
+            if j == n:
+                count += 1
+                break
+            elif j > n:
+                break
+            m1 += 1
+            j += m1
+
+    return count
