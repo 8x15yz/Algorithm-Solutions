168f9b2b70bb25b6667b0c265ad17bc110927434
Jay Kim <8x15yz@gmail.com>
2023-03-03 13:23:19 +0900

    Update 다음큰숫자.py

--- 2023/programers/03/다음큰숫자.py
+++ 2023/programers/03/다음큰숫자.py
@@ -17,3 +17,23 @@ def solution(n):
                 answer = n
                 break
     return answer
+
+
+#완전바보 스트링화 안시킴
+def solution(n):
+    answer = 0
+    cnt = 0
+    for i in bin(n)[2:]:
+        if i == '1':
+            cnt += 1
+    while True:
+        n += 1
+        ct = 0
+        for j in bin(n)[2:]:
+            if j == '1':
+                ct += 1
+        else:
+            if ct == cnt:
+                answer = n
+                break
+    return answer
