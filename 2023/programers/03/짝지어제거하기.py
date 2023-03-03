9faef4fa451562dae1276e195b784228097ce29e
Jay Kim <8x15yz@gmail.com>
2023-03-03 13:43:41 +0900

    Create 짝지어제거하기.py

---
+++ 2023/programers/03/짝지어제거하기.py
@@ -0,0 +1,31 @@
+#
+# 이거 골치아픈 문제네 ..
+# 틀린코드
+def solution(s):
+    answer = 0
+    i = 0
+    while True:
+        slen = len(s)
+        if s == '':
+            answer = 1
+            break
+        elif len(s) == 1:
+            answer = 0
+            break
+            
+        if i == slen:
+            i = 0
+            continue
+            
+        if s[i] == s[i+1]:
+            s = s[:i]+s[i+2:]
+        i += 1
+        
+        for i in range(len(s)-1):
+            if s[i] == s[i+1]:
+                break
+        else:
+            answer = 0
+            break
+            
+    return answer
