3fe6060e9411dd07c349aed1d810bd574340b164
Jay Kim <8x15yz@gmail.com>
2023-03-03 13:56:18 +0900

    Update 짝지어제거하기.py

--- 2023/programers/03/짝지어제거하기.py
+++ 2023/programers/03/짝지어제거하기.py
@@ -29,3 +29,38 @@ def solution(s):
             break
             
     return answer
+
+# 어디서 오타나는지는 알았는데 이제는 시간초과 코드
+def solution(s):
+    answer = 0
+    i = 0
+    while True:
+        slen = len(s)
+        if s == '':
+            answer = 1
+            break
+        elif len(s) == 1:
+            answer = 0
+            break
+            
+        if i == slen:
+            i = 0
+            continue
+            
+        if s[i] == s[i+1]:
+            s = s[:i]+s[i+2:]
+        i += 1
+        
+        for i in range(len(s)-1):
+            if s[i] == s[i+1]:
+                break
+        else:
+            if s == '':
+                answer = 1
+            else:
+                answer = 0
+            break
+            
+    return answer
+
+
