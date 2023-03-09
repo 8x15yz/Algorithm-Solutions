8de79f30fce45f3c0c6168ef35ab74726f5847c9
Jay Kim <8x15yz@gmail.com>
2023-03-09 17:21:15 +0900

    Update 예상대진표.py

--- 2023/programers/03/예상대진표.py
+++ 2023/programers/03/예상대진표.py
@@ -1,4 +1,4 @@
-# 
+# https://school.programmers.co.kr/learn/courses/30/lessons/12985
 #아니 이거 뭐임..? 
 def solution(t,a,b):
     answer = 0
@@ -24,5 +24,28 @@ def solution(t,a,b):
             print(cnt)
             break
     return answer
+
   
-  # 머리 왜이렇게 안돌아가지??
+  # 또틀림
+def solution(t,a,b):
+    answer = 0
+    # 이진탐색으로 풀기
+    al, ar, bl, br, n, cnt = 0, t, 0, t, t, 0
+    while True:
+        cnt += 1
+        n //= 2
+        if n >= a:
+            ar, al = ar-n, al
+        else:
+            ar, al = ar, al+n+1
+        if n >= b:
+            br, bl = br-n, bl
+        else:
+            br, bl = br, bl+n+1
+            
+        if al != bl or ar != br or n == 1:
+            break
+    for i in range(t//2):
+        if 2**i == t:
+            answer = i-cnt+1
+    return answer
