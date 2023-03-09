aeacdd226f260bdd9e2628ed1831b19f796ff261
Jay Kim <8x15yz@gmail.com>
2023-03-09 11:25:02 +0900

    Update 구명보트.py

--- 2023/programers/03/구명보트.py
+++ 2023/programers/03/구명보트.py
@@ -17,3 +17,29 @@ def solution(people, limit):
         else:
             weight += wait
     return answer
+
+
+# ㅠㅠㅠ 틀린답
+def solution(people, limit):
+    answer = 1
+    people = sorted(people, reverse=True)
+    weight = 0
+    cnt = 0
+    while people:
+        if cnt <= 1:
+            wait = people.pop()
+            cnt += 1
+            if weight + wait == limit:
+                cnt = 0
+                weight = 0
+                answer += 1
+            elif weight + wait > limit:
+                ant = 0
+                weight = wait
+                answer += 1
+            else:
+                weight += wait
+        else:
+            # print(cnt)
+            cnt = 0
+    return answer
