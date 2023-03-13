a1a8cffa48ce1f8d16f5355640b6a6928999af86
Jay Kim <8x15yz@gmail.com>
2023-03-13 13:48:51 +0900

    Update n개의최소공배수.py

--- 2023/programers/03/n개의최소공배수.py
+++ 2023/programers/03/n개의최소공배수.py
@@ -1,2 +1,29 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12953
 
+## 시간초과 코드 ..
+def solution(arr):
+    answer = 1
+    comblist = [[] for _ in range(4)]
+    prime = [2, 3, 5, 7]
+    
+    for number in arr:
+        primelist = {2:0, 3:0, 5:0, 7:0}
+        num = number
+        # 소수들로만 이루어진 조합 리스트를 만들기
+        while num != 1:
+            for i in prime:
+                if num%i == 0:
+                    num //= i
+                    # comb += str(i)
+                    primelist[i] += 1
+                else:
+                    continue
+        for s in range(4):
+            comblist[s].append(primelist[prime[s]])
+    
+    for c in range(4):
+        answer *= prime[c]**max(comblist[c])
+    return answer
+  
+  
+## 
