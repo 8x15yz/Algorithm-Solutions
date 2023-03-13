244900ac76397fbf50c5710af08bc52fa35bc5a5
Jay Kim <8x15yz@gmail.com>
2023-03-13 14:35:58 +0900

    Update 점프와순간이동.py

--- 2023/programers/03/점프와순간이동.py
+++ 2023/programers/03/점프와순간이동.py
@@ -1,2 +1,20 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12980
+def solution(n):
+    ans = 0
+    # 이거 dp => 점화식을 만들어야됨
+    dp = [0, 1, 1, 2, 1]
+    if n <= 4:
+        ans = dp[n]
+    else:
+        for i in range(5, n+1):
+            if i%2 == 0:
+                dp.append(dp[i//2])
+            else:
+                dp.append(dp[i-1]+1)
+        ans = dp[-1]
+    return ans
+ 
+## 시간초과함 .. 아놔
+
+## 정답코드
 
