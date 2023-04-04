d9d56fc0d2898d432f754cbc328a905c714f9edf
김현주 <8x15yz@gmail.com>
2023-04-04 10:44:29 +0900

    Update 소수찾기.py

--- 2023/programers/04/소수찾기.py
+++ 2023/programers/04/소수찾기.py
@@ -16,3 +16,23 @@ def solution(numbers):
                 if x != 1 and x != 0: prime.add(x)
 
     return len(prime)
+
+
+# 맞은코드
+from itertools import permutations
+from math import sqrt
+
+def solution(numbers):
+    numlen = len(numbers)
+    prime = set()
+    for i in range(1, numlen+1):
+        permu = permutations(list(numbers), i)
+        for x in list(set(permu)):
+            x = int(''.join(list(x)))
+            if x == 1 or x == 0: continue
+            for j in range(2, int(sqrt(x))+1):
+                if x%j == 0: break
+            else:
+                prime.add(x)
+
+    return len(prime)
