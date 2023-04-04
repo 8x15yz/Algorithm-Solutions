02cb2e000f9fc99c4ec974845a33ed9c2c4fdc4a
김현주 <8x15yz@gmail.com>
2023-04-04 10:10:42 +0900

    Update 소수찾기.py

--- 2023/programers/04/소수찾기.py
+++ 2023/programers/04/소수찾기.py
@@ -1,16 +1,15 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/42839
 
-from itertools import permutations
 
+from itertools import permutations
 def solution(numbers):
     answer = 0
     numlen = len(numbers)
     prime = set()
     for i in range(1, numlen+1):
         permu = permutations(list(numbers), i)
-        for x in list(permu):
+        for x in list(set(permu)):
             x = int(''.join(list(x)))
-            # print(x)
             for j in range(x-1, 1, -1):
                 if x%j == 0: break
             else:
