63cb578f12abd26b556e15044c1b00f80e587095
김현주 <8x15yz@gmail.com>
2023-04-05 14:08:58 +0900

    Update 캐시.py

--- 2023/programers/04/캐시.py
+++ 2023/programers/04/캐시.py
@@ -17,3 +17,24 @@ def solution(cacheSize, cities):
             # print(5)
     
     return answer
+
+
+# cache 사이즈가 0인 경우의 예외처리 + 도시명을 모두 lowercase로 변형해 적용한 정답코드
+def solution(cacheSize, cities):
+    if cacheSize == 0: 
+        return len(cities)*5
+    
+    else:
+        answer = 5
+        cache = [cities.pop(0).lower()]
+
+        for page in cities:
+            page = page.lower()
+            if page in cache:
+                cache.remove(page)
+                cache = [page]+cache
+                answer += 1
+            else:
+                cache = [page]+cache[:cacheSize-1]
+                answer += 5
+        return answer
