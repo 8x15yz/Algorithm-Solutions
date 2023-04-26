d655d5376a5def0c60dbaf367525f5b9c4af92af
김현주 <8x15yz@gmail.com>
2023-04-26 10:22:07 +0900

    Update 파일명정렬.py

--- 2023/programers/04/파일명정렬.py
+++ 2023/programers/04/파일명정렬.py
@@ -25,3 +25,29 @@ def solution(realfiles):
     
     for file in files: answer.append(realfiles[file[-1]])
     return answer
+
+
+
+## 정답 => 바로 끊어주는것보다 인덱스를 가져와서 슬라이싱 해주는게 더 안전함
+def solution(realfiles):
+    files = realfiles.copy()
+    answer = []
+    fileDict = {}
+    cnt = 0
+    HeadTail = []
+    for i in range(len(files)):
+        view = files[i]
+        for j in range(len(view)):
+            if j+1 < len(view) and view[j].isdigit() != view[j+1].isdigit():
+                HeadTail.append(j+1)
+                cnt += 1
+        else: 
+            if len(HeadTail) == 1:
+                files[i] = [view[:HeadTail[0]].lower(), int(view[HeadTail[0]:]), i]
+            else:
+                files[i] = [view[:HeadTail[0]].lower(), int(view[HeadTail[0]:HeadTail[1]]), i]
+            cnt, HeadTail = 0, []
+
+    files = sorted(files, key = lambda x: (x[0] , x[1]))
+    for file in files: answer.append(realfiles[file[-1]])
+    return answer
