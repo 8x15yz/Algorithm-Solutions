bd65f59825077ec9575f56ed6584823a0e16583f
김현주 <8x15yz@gmail.com>
2023-04-27 10:56:57 +0900

    Update 프렌즈4블록.py

--- 2023/programers/04/프렌즈4블록.py
+++ 2023/programers/04/프렌즈4블록.py
@@ -1,29 +1,65 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/17679
 
 deltas = [[(-1, 0), (-1, 1), (0, 1)], [(0, 1), (1, 1), (1, 0)], [(1, 0), (1, -1), (0, -1)], [(0, -1), (-1, -1), (-1, 0)]]
-def solution(M, N, board):
-    answer = 0
-    # 1 턴 구현
-    board = list(map(list, board))
-    check = [[0]*N]*M
-    for a in board:
-        print(a)
-    print()
+def matchingBlock(board, N, M, answer):
+    checkBox = []
     for m in range(M):
         for n in range(N):
             for delta in deltas:
-                checkBox = []
+                checkBoxInner = []
                 for dm, dn in delta:
                     if 0 <= dm+m < M and 0 <= dn+n <N and board[dm+m][dn+n] == board[m][n]:
-                        # print(dm+m, dn+n, board[dm+m][dn+n])
-                        checkBox.append((dm+m, dn+n))
+                        checkBoxInner.append((m, n))
+                        checkBoxInner.append((dm+m, dn+n))
                     else: break
                 else:
-                    print(m, n, checkBox)
-                    for i, j in checkBox:
-                        check[i][j] = 1
+                    checkBox += checkBoxInner
+    else:
+        for i, j in checkBox:
+            if board[i][j] != '0':
+                board[i][j] = '0'
+                answer += 1
+    return (board, answer)
+
+def relocatingBlock(board, N, M):
+    for n in range(N):
+        for _ in range(M):
+            for m in range(1, M):
+                if board[m][n] == '0':
+                    board[m][n], board[m-1][n] = board[m-1][n], board[m][n]
+    return board
+
+def solution(M, N, board):
+    answer = 0
+    board = list(map(list, board))
     
-    for a in check:
-        print(a)
-                        
+    while True:
+        matchResult, answer = matchingBlock(board, N, M, answer)
+        relocateResult = relocatingBlock(matchResult.copy(), N, M)
+        if matchResult == relocateResult: break
+        else: board = relocateResult     
     return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
