98e4aa36736f0a992d0bb82e7e21b0b9a3270ccf
김현주 <8x15yz@gmail.com>
2023-04-28 13:27:34 +0900

    Update 메뉴리뉴얼.py

--- 2023/programers/04/메뉴리뉴얼.py
+++ 2023/programers/04/메뉴리뉴얼.py
@@ -1,16 +1,39 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/72411
 
+from itertools import combinations
+
+def isDuplicate(dishes, compareTuple):
+    for dish in dishes:
+        for d in compareTuple:
+            if d not in dish: break
+        else: return 'already'
+    else: return ''.join(compareTuple)
+
 def solution(orders, course):
     answer = []
-    foodDict = {}
-    for d in range(len(orders)):
-        for dish in orders[d]:
-            if dish not in foodDict: foodDict[dish] = [d+1]
-            else: foodDict[dish].append(d+1)
+    dishD = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9,
+             'K': 10, 'L': 11, 'M': 12, 'N': 13, 'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
+             'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
     
-    # 교집합이 course개인  문자들의 조합을 모두 출력
-    # 인 것 같은데?
+    orderList = [[0]*26 for _ in range(len(orders))]
     
+    for o in range(len(orders)):
+        for order in orders[o]:
+            orderList[o][dishD[order]] = 1
+    course = sorted(course, reverse=True)
+    for i in course:
+        for alpha in list(combinations(dishD, i)):
+            cnt = 0
+            for j in range(len(orderList)):
+                combi = 1
+                for di in alpha:
+                    combi *= orderList[j][dishD[di]]
+                    if combi == 0: break
+                else:
+                    cnt += 1
+                    if cnt > 1:
+                        isDup = isDuplicate(answer, alpha)
+                        if isDup != 'already': answer.append(isDup)
+                        break
     
-    print(foodDict)
     return answer
