c10ccbc13a3264cd90f6d941dd47d5bdb616f717
김현주 <8x15yz@gmail.com>
2023-05-15 10:00:15 +0900

    Update 연속부분수열합의개수.py

--- 2023/programers/05/연속부분수열합의개수.py
+++ 2023/programers/05/연속부분수열합의개수.py
@@ -17,3 +17,20 @@ def solution(elements):
         break
     
     return answer
+
+
+## 풀이완
+def solution(elements):
+    sumSet = set()
+    for i in range(1, len(elements)+1):
+        sp = 0
+        ep = sp+i
+        cnt = 0
+        while cnt < len(elements):
+            if ep <= sp: sumSet.add(sum(elements[sp:])+sum(elements[:ep]))
+            else: sumSet.add(sum(elements[sp:ep]))
+            sp = (sp+1)%len(elements)
+            ep = (ep+1)%len(elements)
+            cnt += 1
+    
+    return len(sumSet)
