3cc0378b2f47d93ba3354ff134fe0623f6dbe917
김현주 <8x15yz@gmail.com>
2023-05-15 10:30:45 +0900

    Update 행렬의곱셈.py

--- 2023/programers/05/행렬의곱셈.py
+++ 2023/programers/05/행렬의곱셈.py
@@ -1,2 +1,14 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12949
 
+def solution(arr1, arr2):
+    answer = []
+    for i in range(len(arr1)):
+        ans = []
+        for k in range(len(arr2[0])):
+            part = 0
+            for j in range(len(arr2)):
+                part += (arr1[i][j]*arr2[j][k])
+            ans.append(part)
+        answer.append(ans)
+    
+    return answer
