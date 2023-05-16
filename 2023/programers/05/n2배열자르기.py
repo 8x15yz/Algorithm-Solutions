d3362ec60300ff75f849eb13fa902babc791fc94
김현주 <8x15yz@gmail.com>
2023-05-16 10:00:26 +0900

    Update n2배열자르기.py

--- 2023/programers/05/n2배열자르기.py
+++ 2023/programers/05/n2배열자르기.py
@@ -12,3 +12,24 @@ def solution(n, left, right):
     for a in answer:
         newans += a
     return newans[left:right+1]
+
+
+
+# 시간초과 ...
+def solution(n, left, right):
+    answer = []
+    endPoint = (right//n+1, right%n+1)
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            answer+= [max(i, j)]
+            if (i, j) == endPoint: 
+                return answer[left:]
+            
+
+# 이거였엌ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ  아놔 ~
+def solution(n, left, right):
+    answer = []
+    for i in range(left, right+1):
+        answer.append(max(i//n+1, i%n+1))
+    return answer
+            
