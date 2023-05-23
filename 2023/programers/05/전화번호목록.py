f44d73f6573e86c580f79d93e8e80e4f4d509c56
김현주 <8x15yz@gmail.com>
2023-05-23 09:47:14 +0900

    Update 전화번호목록.py

--- 2023/programers/05/전화번호목록.py
+++ 2023/programers/05/전화번호목록.py
@@ -9,3 +9,17 @@ def solution(phone_book):
             if ber == num: continue
             elif ber[:len(num)] == num: return False
     return answer
+
+
+# 신기하다 딕셔너리로 푸니까 효율성 통과함
+def solution(phone_book):
+    answer = True
+    phoneDict = {}
+    
+    for num in phone_book:
+        phoneDict[num] = ''
+    
+    for num in phone_book:
+        for i in range(1, len(num)):
+            if num[:i] in phoneDict: return False
+    return answer
