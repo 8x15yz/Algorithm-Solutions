c320e3a4a80e3dca208fbd53fb9caf9d09f7a2f5
김현주 <8x15yz@gmail.com>
2023-05-24 11:08:16 +0900

    Update 더맵게.py

--- 2023/programers/05/더맵게.py
+++ 2023/programers/05/더맵게.py
@@ -1,5 +1,6 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3
 
+# 난장판코드
 def solution(scoville, K):
     answer = 0
     while True:
@@ -9,3 +10,18 @@ def solution(scoville, K):
             scoville = [scoville[0]*scoville[1]*2] + scoville[2:]
             answer += 1
         else: return answer
+
+
+# 정확성만점인데 효율성거지
+def solution(scoville, K):
+    answer = 0
+    while len(scoville) > 1:
+        scoville.sort()
+        if scoville[0] >= K: return answer
+        scoville = [scoville[0]+scoville[1]*2] + scoville[2:]
+        answer += 1
+        
+    if scoville[0] < K: return -1
+    else: return answer
+   
+
