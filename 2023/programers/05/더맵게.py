cf93fae5992cf037a57504d6e61bc4199eab0316
김현주 <8x15yz@gmail.com>
2023-05-24 14:11:36 +0900

    Update 더맵게.py

--- 2023/programers/05/더맵게.py
+++ 2023/programers/05/더맵게.py
@@ -25,3 +25,33 @@ def solution(scoville, K):
     else: return answer
    
 
+
+# 힙으로 만들고 있는중
+def heap(sco):
+    vNode = 1
+    while vNode**2 < len(sco):
+        if sco[vNode*2] < sco[vNode*2+1] and sco[vNode*2] < sco[vNode]: 
+            sco[vNode], sco[vNode*2] = sco[vNode*2], sco[vNode]
+            vNode = vNode*2
+        elif sco[vNode*2+1] < sco[vNode*2] and sco[vNode*2+1] < sco[vNode]: 
+            sco[vNode], sco[vNode*2+1] = sco[vNode*2+1], sco[vNode]
+            vNode = vNode*2+1
+    return sco
+
+def solution(sco, K):
+    answer = 0
+    sco += [0]
+    sco.sort()
+    sSco = [0, 0]
+    if sco[1] < K: 
+        for i in range(2):
+            sSco[i], sco[1] = sco[1], sco.pop() # 삭제해서 루트로 올림
+            heap(sco)
+            
+    sumSco = sSco[0]+sSco[1]*2
+    sco.append(sumSco)
+    print(sumSco, sco)
+    
+    
+    
+    
