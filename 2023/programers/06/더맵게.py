336afdc2b914a33228d22e7ac3716b9c4b887607
Jay <8x15yz@gmail.com>
2023-06-07 09:40:00 +0900

    Update 더맵게.py

--- 2023/programers/06/더맵게.py
+++ 2023/programers/06/더맵게.py
@@ -1,2 +1,19 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/42626
+import heapq
+def solution(scoville, K):
+    answer = 0
+    scoville = sorted(scoville)
+    while scoville[0] < K:
+        if len(scoville) == 1: 
+            if scoville[0] < K: answer = -1
+            break
+        else:
+            a = heapq.heappop(scoville)
+            b = heapq.heappop(scoville)
+            heapq.heappush(scoville, a+b*2)
+            answer += 1
+    return answer
+  
+  
+  
 
