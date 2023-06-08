f7e2e6ef1818ed90cea6ae4e56832817a17bcb7f
Jay <8x15yz@gmail.com>
2023-06-08 16:16:48 +0900

    Update 땅따먹기.py

--- 2023/programers/06/땅따먹기.py
+++ 2023/programers/06/땅따먹기.py
@@ -20,3 +20,51 @@ def solution(land):
             answer = dp[-1] if answer < dp[-1] else answer
     
     return answer
+
+
+# 계속 푸는중
+def solution(land):
+    # land = [[4, 3, 2, 1], [2, 2, 3, 3], [1, 1, 1, 1], [3, 4, 5, 6], [1, 1, 1, 1], [6, 6, 6, 3], [7, 8, 6, 5]]
+    # land = [[4, 3, 2, 1], [2, 2, 2, 1], [6, 6, 6, 4], [8, 7, 6, 5]]
+    answer = [0 for _ in range(len(land))]
+    path = [-1 for _ in range(len(land))]
+    basket = []
+    duplicateFlag = -2
+    for i in range(len(land)):
+        # print(i, duplicateFlag)
+        comp = -1
+        duplicateCheck = [0, 0, 0, 0]
+        for j in range(4):
+            if land[i].count(land[i][j]) > 1 and land[i][j] >= comp:
+                duplicateFlag = i-1 if duplicateFlag == -2 else duplicateFlag
+                if land[i][j] > comp: duplicateCheck = [0, 0, 0, 0]
+                duplicateCheck[j], comp = 1, land[i][j]
+                    
+            elif land[i][j] > comp and i == 0 or land[i][j] > comp and j != path[i-1]:
+                path[i], comp = j, land[i][j]
+                duplicateCheck = [0, 0, 0, 0]
+                
+        else:
+            # print(comp, duplicateCheck)
+            if sum(duplicateCheck) == 0: 
+                answer[i] = comp
+                if duplicateFlag != -2:
+                    basket[0][path[duplicateFlag]] = 0
+                    basket[-1][path[i]] = 0
+                    cRange = i-duplicateFlag-1
+                    # print(i, duplicateFlag)
+                    for k in range(cRange):
+                        # print(k)
+                        for l in range(4):
+                            if basket[k][l] == 1:
+                                path[k] = l
+                                answer[k+1] = land[duplicateFlag+1+k][l]
+                                # print(duplicateFlag+1+k, answer)
+                                if k < cRange-1: basket[k+1][l] = 0
+                                break
+                    else:
+                        duplicateFlag = -2
+            else: basket.append(duplicateCheck)
+    #         print()
+    # print(answer, path)     
+    return sum(answer)
