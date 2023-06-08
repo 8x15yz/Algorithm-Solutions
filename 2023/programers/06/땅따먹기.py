f13a4d0ca8c4470fb036dea73dbc2715dc87de39
Jay <8x15yz@gmail.com>
2023-06-08 10:01:30 +0900

    Update 땅따먹기.py

--- 2023/programers/06/땅따먹기.py
+++ 2023/programers/06/땅따먹기.py
@@ -1,18 +1,22 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12913
 def solution(land):
     answer = 0
-    # 같은 열 연속 불가
+    # 같은 열 연속 불rk
     L = len(land)
-    dp = [0 for _ in range(L)]
-    checkL = -1
-    for i in range(L):
-        maxL = -1
-        for j in range(4):
-            if maxL < land[i][j] and j != checkL: 
-                maxL = land[i][j]
-                fCheck = j
-        else: 
-            checkL = fCheck
-            dp[i] = maxL if i == 0 else dp[i-1] + maxL
+    for c in range(4):
+        dp = [0 for _ in range(L)]
+        checkL = c
+        dp[0] = land[0][c]
+        for i in range(1, L):
+            maxL = -1
+            for j in range(4):
+                if maxL < land[i][j] and j != checkL: 
+                    maxL = land[i][j]
+                    fCheck = j
+            else: 
+                checkL = fCheck
+                dp[i] = dp[i-1] + maxL
+        else:
+            answer = dp[-1] if answer < dp[-1] else answer
     
-    return dp[-1]
+    return answer
