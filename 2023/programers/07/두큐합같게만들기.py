155ab913cfd863ebbb87df9b2b08fd5dcd1acaee
Jay <8x15yz@gmail.com>
2023-07-25 11:11:53 +0900

    Update 두큐합같게만들기.py

--- 2023/programers/07/두큐합같게만들기.py
+++ 2023/programers/07/두큐합같게만들기.py
@@ -1,4 +1,5 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/118667
+
 def solution(queue1, queue2):
     answer = 0
     st = len(queue1)
@@ -6,9 +7,7 @@ def solution(queue1, queue2):
     end = len(queue1)-1
     goal = sum(queue1)//2
     state = sum(queue2)
-    # while True:
-    for _ in range(50000):
-        # print(st, end, state, goal)
+    while answer < 1000000:
         if state == goal:
             return answer
         elif state > goal:
@@ -19,6 +18,8 @@ def solution(queue1, queue2):
             state += queue1[end]
         answer += 1
         
-            
     
     return -1
+
+
+# 음 .. 정말 못생긴 코드인데 일단 풀렸다
