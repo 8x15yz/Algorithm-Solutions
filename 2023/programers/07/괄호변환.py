3ceb54aebb3be38029ab30b7eed00877c56c0b7a
Jay <8x15yz@gmail.com>
2023-07-25 14:08:46 +0900

    Update 괄호변환.py

--- 2023/programers/07/괄호변환.py
+++ 2023/programers/07/괄호변환.py
@@ -1,11 +1,10 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/60058
-#푸는중
 
 def isRight(u):
     stack = []
     for i in u:
         if i == '(': stack.append(i)
-        elif i == ')' and !stack: return False
+        elif i == ')' and not stack: return False
         else: stack.pop()
     else: return True
 
@@ -16,14 +15,14 @@ def solution(p):
         while True:
             if lft > 0 and rgt == lft:
                 u, v = p[:i], p[i:]
-                if isRight(u): return u+solution(v)
+                if isRight(u): 
+                    return u+solution(v)
                 else:
-                    u = u[1:len(u)-1]
-                    retans = '('
-                    for j in u:
-                        if j == '(': retans += ')'
-                        else: retans += '('
-                    return retans+solution(v)
-            if p[i] == ')': rgt += 1
-            else: lft += 1
-            i += 1
+                    u, retans = u[1:len(u)-1], ''
+                    for j in u: 
+                        retans = retans+')' if j == '(' else retans+'('
+                    return '('+solution(v)+')'+retans
+            else:
+                if p[i] == ')': rgt += 1
+                else: lft += 1
+                i += 1
