efbf473e9368e1a6c44225bae045ec7e3942a2b9
Jay <8x15yz@gmail.com>
2023-07-26 13:09:06 +0900

    Update 수식최대화.py

--- 2023/programers/07/수식최대화.py
+++ 2023/programers/07/수식최대화.py
@@ -1,27 +1,42 @@
-#https://school.programmers.co.kr/learn/courses/30/lessons/67257
+# https://school.programmers.co.kr/learn/courses/30/lessons/67257
+# 진짜드럽게풀엇음 ..
 
-def calculate(a, b, cal):
-    if cal == "*": return a*b
-    elif cal == "+": return a+b
-    else cal == "-": return a-b
-    
+def cal(a, b, cal):
+    if cal == "-": return str(int(a)-int(b))
+    elif cal == "*": return str(int(a)*int(b))
+    else: return str(int(a)+int(b))
+
 from itertools import permutations
-
-def solution(expression):
-    calset = list(permutations(["+", "-", "*"], 3))
-    for cal in calset:
-        for c in cal:
+def solution(source):
+    maxnum = 0
+    for calset in list(permutations(["-", "+", "*"], 3)):
+        exp = source + ')'
+        for c in calset:
+            lenexp = len(exp)
+            answer = 0
+            i = 0
+            newexp = ''
             num = ""
-            for e in range(len(expression)):
-                if expression[e] == c:
-                    backward, forward = "", ""
-                    for j in range(e+1, len(expression)):
-                        if not e.isdigit():
-                            forward = str(calculate(int(num), backward, c))
+            while i < lenexp:
+                if num != "" and exp[i] == c: 
+                    forward = num
+                    num = ""
+                    j = i+1
+                    while j < lenexp:
+                        if num != "" and not exp[j].isdigit():
+                            backward = num
+                            num = cal(forward, backward, c)
+                            i = j-1
                             break
-                        else: backward += expression[e]
-                elif not e.isdigit(): num = ""
-                    
-                    
-             
-    return answer
+                        else: num += exp[j]
+                        j += 1
+                elif num != "" and not exp[i].isdigit():
+                    newexp += num + exp[i]
+                    num = ""
+                else: num += exp[i]
+
+                i += 1
+            exp = newexp
+        else:
+            if maxnum < abs(int(newexp[:-1])): maxnum = abs(int(newexp[:-1]))
+    return maxnum
