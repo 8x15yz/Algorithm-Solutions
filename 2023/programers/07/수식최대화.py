571a0dbab8c606d0aee2ddd29c9d755d226c83e5
Jay <8x15yz@gmail.com>
2023-07-26 10:05:15 +0900

    Update 수식최대화.py

--- 2023/programers/07/수식최대화.py
+++ 2023/programers/07/수식최대화.py
@@ -1,21 +1,27 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/67257
 
+def calculate(a, b, cal):
+    if cal == "*": return a*b
+    elif cal == "+": return a+b
+    else cal == "-": return a-b
+    
 from itertools import permutations
 
 def solution(expression):
     calset = list(permutations(["+", "-", "*"], 3))
-    answer = 0
-    num = ""
-    pblm = []
-    
-    for e in expression:
-        if not e.isdigit():
-            pblm.append(int(num))
-            pblm.append(e)
+    for cal in calset:
+        for c in cal:
             num = ""
-        else:
-            num += e
-    else: pblm.append(int(num))
-    
-    print(pblm)
+            for e in range(len(expression)):
+                if expression[e] == c:
+                    backward, forward = "", ""
+                    for j in range(e+1, len(expression)):
+                        if not e.isdigit():
+                            forward = str(calculate(int(num), backward, c))
+                            break
+                        else: backward += expression[e]
+                elif not e.isdigit(): num = ""
+                    
+                    
+             
     return answer
