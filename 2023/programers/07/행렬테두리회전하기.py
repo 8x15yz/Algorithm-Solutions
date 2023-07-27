d39d2e5dc9ce98b0218e427664e2e0d692d9aa6f
Jay <8x15yz@gmail.com>
2023-07-27 16:50:30 +0900

    Update 행렬테두리회전하기.py

--- 2023/programers/07/행렬테두리회전하기.py
+++ 2023/programers/07/행렬테두리회전하기.py
@@ -22,33 +22,62 @@ def solution(rows, cols, queries):
     
     return answer
 # 이건 글러먹은 코드다 처음부터 다시 쓰기
-
 def viewArr(arr):
     for a in arr:
         print(a)
         
 def solution(rows, cols, queries):
+    rows, cols, queries = 6, 6, [[2, 2, 5, 4]]
     answer = []
-    arr = [[i*cols+j+1 for j in range(cols)] for i in range(rows)]
+    arr = [['{0:02d}'.format(i*cols+j+1) for j in range(cols)] for i in range(rows)]
     for que in queries:
-        note1, note2, note3, note4 = 0, 0, 0, 0
+        note4 = arr[que[0]-1][que[1]-1]
         for j in range(que[3]-1, que[1]-1, -1):
-            print(arr[que[0]-1][j], arr[que[0]-1][j-1])
-        # viewArr(arr)
+            print(arr[que[0]-1][j])
+            if j == que[1]: arr[que[0]-1][j] = note4
+            else:
+                if j == que[3]-1 : note1 = arr[que[0]-1][j]
+                arr[que[0]-1][j] = arr[que[0]-1][j-1]
+            
+            
+        viewArr(arr)
+        #print(note)
         print()
         for j in range(que[2]-1, que[0]-1, -1):
-            print(arr[j][que[3]-1], arr[j-1][que[3]-1])
-        # viewArr(arr)
+            print(arr[j][que[3]-1])
+            if j == que[0]: arr[j][que[3]-1] = note1
+            else:
+                if j == que[2]-1 : note2 = arr[j][que[3]-1]
+                arr[j][que[3]-1] = arr[j-1][que[3]-1]
+            
+        viewArr(arr)
+        #print(note)
         print()
         for j in range(que[1]-1, que[3]-1):
-            print(arr[que[2]-1][j], arr[que[2]-1][j+1])
-        # viewArr(arr)
+            print(arr[que[2]-1][j])
+            if j == que[3]: arr[que[2]-1][j] = note2
+            else:
+                if j == que[1]-1 : note3 = arr[que[2]-1][j]
+                arr[que[2]-1][j] = arr[que[2]-1][j+1]
+            
+        viewArr(arr)
+        #print(note)
         print()
         for j in range(que[0]-1, que[2]-1):
-            print(arr[j][que[1]-1], arr[j+1][que[1]-1])
-        # viewArr(arr)
-        print()
+            print(arr[j][que[1]-1])
+            if j == que[2]: arr[j][que[1]-1] = note3
+            else:
+                #if j == que[0]-1 : note = arr[j][que[1]-1]
+                arr[j][que[1]-1] = arr[j+1][que[1]-1]
+            
+        viewArr(arr)
+        #print(note)
+        print('*')
         
+        
+        
+            
+    return answer
         
         
             
