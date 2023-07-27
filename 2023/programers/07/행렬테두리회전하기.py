c6ed97cafc61c3ec45f1962f65ff40c7f7363517
Jay <8x15yz@gmail.com>
2023-07-27 17:25:27 +0900

    Update 행렬테두리회전하기.py

--- 2023/programers/07/행렬테두리회전하기.py
+++ 2023/programers/07/행렬테두리회전하기.py
@@ -21,13 +21,10 @@ def solution(rows, cols, queries):
             answer.append(min)
     
     return answer
+
+
 # 이건 글러먹은 코드다 처음부터 다시 쓰기
-def viewArr(arr):
-    for a in arr:
-        print(a)
-        
 def solution(rows, cols, queries):
-    rows, cols, queries = 3, 3, [[1, 1, 2, 2], [1, 2, 2, 3], [2, 1, 3, 2], [2, 2, 3, 3]]
     answer = []
     note1, note2, note3, note4= 0, 0, 0, 0
     arr = [['{0:02d}'.format(i*cols+j+1) for j in range(cols)] for i in range(rows)]
@@ -41,42 +38,29 @@ def solution(rows, cols, queries):
                 if j == que[3]-1 : note1 = arr[que[0]-1][j]
                 arr[que[0]-1][j] = arr[que[0]-1][j-1]
             
-            
-        #viewArr(arr)
-        #print(note)
-        #print()
         for j in range(que[2]-1, que[0]-1, -1):
             min = int(arr[j][que[3]-1]) if int(arr[j][que[3]-1]) < min else min
             if j == que[0]: arr[j][que[3]-1] = note1
             else:
                 if j == que[2]-1 : note2 = arr[j][que[3]-1]
                 arr[j][que[3]-1] = arr[j-1][que[3]-1]
-            
-        #viewArr(arr)
-        #print(note)
-        #print()
+
         for j in range(que[1]-1, que[3]-1):
             min = int(arr[que[2]-1][j]) if int(arr[que[2]-1][j]) < min else min
             if j == que[3]-2: arr[que[2]-1][j] = note2
             else:
                 if j == que[1]-1 : note3 = arr[que[2]-1][j]
                 arr[que[2]-1][j] = arr[que[2]-1][j+1]
-            
-        #viewArr(arr)
-        #print(note)
-        #print()
+
         for j in range(que[0]-1, que[2]-1):
             min = int(arr[j][que[1]-1]) if int(arr[j][que[1]-1]) < min else min
             if j == que[2]-2: arr[j][que[1]-1] = note3
             else:
-                #if j == que[0]-1 : note = arr[j][que[1]-1]
                 arr[j][que[1]-1] = arr[j+1][que[1]-1]
-            
-        #viewArr(arr)
-        #print(note)
-        #print('*')
+
         answer.append(min)
         
         
             
     return answer
+# 난 이것도 글러먹었다고 생각해 
