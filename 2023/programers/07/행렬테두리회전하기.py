29a4fb23698e46fef4bf64da78f1e3b376861004
Jay <8x15yz@gmail.com>
2023-07-27 14:29:04 +0900

    Update 행렬테두리회전하기.py

--- 2023/programers/07/행렬테두리회전하기.py
+++ 2023/programers/07/행렬테두리회전하기.py
@@ -22,3 +22,34 @@ def solution(rows, cols, queries):
     
     return answer
 # 이건 글러먹은 코드다 처음부터 다시 쓰기
+
+def viewArr(arr):
+    for a in arr:
+        print(a)
+        
+def solution(rows, cols, queries):
+    answer = []
+    arr = [[i*cols+j+1 for j in range(cols)] for i in range(rows)]
+    for que in queries:
+        note1, note2, note3, note4 = 0, 0, 0, 0
+        for j in range(que[3]-1, que[1]-1, -1):
+            print(arr[que[0]-1][j], arr[que[0]-1][j-1])
+        # viewArr(arr)
+        print()
+        for j in range(que[2]-1, que[0]-1, -1):
+            print(arr[j][que[3]-1], arr[j-1][que[3]-1])
+        # viewArr(arr)
+        print()
+        for j in range(que[1]-1, que[3]-1):
+            print(arr[que[2]-1][j], arr[que[2]-1][j+1])
+        # viewArr(arr)
+        print()
+        for j in range(que[0]-1, que[2]-1):
+            print(arr[j][que[1]-1], arr[j+1][que[1]-1])
+        # viewArr(arr)
+        print()
+        
+        
+        
+            
+    return answer
