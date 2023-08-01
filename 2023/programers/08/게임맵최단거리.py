e14fe6f662cff3cd80a4dd747bc12f8f19a11d69
Jay <8x15yz@gmail.com>
2023-08-01 14:15:50 +0900

    Update 게임맵최단거리.py

--- 2023/programers/08/게임맵최단거리.py
+++ 2023/programers/08/게임맵최단거리.py
@@ -19,3 +19,27 @@ def solution(maps):
                                 stack.append((si+di, sj+dj, route+1))
                                 
     return answer 
+
+
+# 아오 ~~~~~ 아직 푸는중
+dt = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+def solution(maps):
+    N, M = len(maps), len(maps[0])
+    visit = [(0, 0)]
+    
+    for i in range(N):
+        for j in range(M):
+            if i == N-1 and j == M-1: return -1
+            visit.append((i, j))
+            queue = [(i, j, 1)]
+            while queue:
+                si, sj, route = queue.pop(0)
+                print(si, sj, route)
+                if si == N-1 and sj == M-1: return route
+                else:
+                    for di, dj in dt:
+                        ni, nj = si+di, sj+dj
+                        if 0 <= ni < N and 0 <= nj < M and maps[ni][nj] and (ni, nj) not in visit:
+                            visit.append((ni, nj))
+                            queue.append((ni, nj, route+1))
