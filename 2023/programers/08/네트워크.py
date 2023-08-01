708fb92912c39f1993ab71b65f133359f25e020d
Jay <8x15yz@gmail.com>
2023-08-01 16:49:37 +0900

    Update 네트워크.py

--- 2023/programers/08/네트워크.py
+++ 2023/programers/08/네트워크.py
@@ -1,17 +1,25 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/43162
 
 def solution(n, computers):
-    answer = 1
-    networks = {i:0 for i in range(len(computers))}
+    answer = 0
+    networks = ["N" for _ in range(len(computers))]
+    visit = [0 for _ in range(len(computers))]
     s = 1
     for i in range(len(computers)):
         for j in range(s, len(computers)):
-            if computers[i][j]:
-                networks[i], networks[j] = 1, 1
+            if computers[i][j]: networks[i] = j
         else: s += 1
-        
-    state = networks[0]
-    for i in range(len(computers)):
-        if networks[i] != state: answer += 1
-        
+    
+    print(networks)
+    for v in range(len(computers)):
+        if visit[v]: continue
+        while True:
+            if networks[v] == "N":
+                answer += 1
+                visit[v] = 1
+                break
+            else: 
+                visit[v] = 1
+                v = networks[v]
+                
     return answer
