92096cd771ea5006f18371ba29c71ca195583e1d
Jay <8x15yz@gmail.com>
2023-08-22 13:16:06 +0900

    Update 거리두기확인하기.py

--- 2023/programers/08/거리두기확인하기.py
+++ 2023/programers/08/거리두기확인하기.py
@@ -1,11 +1,11 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/81302
 
+# 이렇게 허술한 코드가 83점?????????
 from itertools import combinations
 dt1 = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-dt2 = [(1, 1), (-1, 1), (1, -1), (-1, -1)]
 
 def solution(places):
-    places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"]]
+    #places = [["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"]]
     answer = []
     for place in places:
         Ps = []
@@ -15,34 +15,31 @@ def solution(places):
                 if place[i][j] == "P": Ps.append((i, j))
                 if place[i][j] == "X": Xs.append((i, j))
         for a, b in list(combinations([i for i in range(len(Ps))], 2)):
+            #print(a, b)
             p1i, p1j = Ps[a]
             p2i, p2j = Ps[b]
             if abs(p1i - p2i) + abs(p1j - p2j) <= 2:
                 
-                ## 델타탐색 : 직선 4방향 1거리
+
                 def recogfir(p1i, p1j, p2i, p2j):
                     for di, dj in dt1:
                         if p1i + di == p2i and p1j + dj == p2j:
-                            return 0
+                            return 0 
+                        elif p1i + 2*di == p2i and p1j + 2*dj == p2j:
+                            if place[p1i+di][p1j+dj] == "O":
+                                return 0
                     else: return 1
                 if recogfir(p1i, p1j, p2i, p2j) == 0:
                     answer.append(0)
                     break
                     
-                ## 델타탐색 : 대각선 4방향
                 def recogsec(p1i, p1j, p2i, p2j):
-                    pass
+                    if p1i + 1 == p2i and p1j + 1 == p2j:
+                        if place[p1i+1][p1j] == "O" or place[p1i][p1j+1] == "O":
+                            return 0
+                    else: return 1
                 if recogsec(p1i, p1j, p2i, p2j) == 0:
                     answer.append(0)
                     break
-                
-                ## 델타탐색 : 직선 4방향 2거리
-                def recogfin(p1i, p1j, p2i, p2j):
-                    pass
-                if recogfin(p1i, p1j, p2i, p2j) == 0:
-                    answer.append(0)
-                    break
-                    
-                
-        print()
+        else: answer.append(1)
     return answer
