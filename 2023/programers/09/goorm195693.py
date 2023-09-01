06171f1568a77777af52a78775466b25e76f7331
Jay <8x15yz@gmail.com>
2023-09-01 15:49:08 +0900

    Create goorm195693.py

---
+++ 2023/programers/09/goorm195693.py
@@ -0,0 +1,15 @@
+#https://level.goorm.io/exam/195693/통증-2
+
+N = int(input())
+A, B = map(int, input().split())
+
+dp = [float('inf')] * (N + 1)
+dp[0] = 0
+
+for i in range(N + 1):
+	if i - A >= 0:
+		dp[i] = min(dp[i], dp[i - A] + 1)
+	if i - B >= 0:
+		dp[i] = min(dp[i], dp[i - B] + 1)
+
+print(dp[N] if dp[N] != float('inf') else -1)
