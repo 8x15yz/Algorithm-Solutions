7494413b3e772d396fbba4c17e456237dba0c3a0
Jay <8x15yz@gmail.com>
2023-09-01 15:45:02 +0900

    Update programers12924.py

--- 2023/programers/09/programers12924.py
+++ 2023/programers/09/programers12924.py
@@ -1,4 +1,5 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/12924
+# https://school.programmers.co.kr/learn/courses/30/lessons/12924 
+# 숫자의 표현
 # set의 원소로 list 는 안들어간다는거 ~~~
 # 기본테스트는 다맞았는데 효율성테스트에서 빵점임 ㅠㅠ 시간초과줄여야댐
 
@@ -22,3 +23,20 @@ def solution(n):
         else: visitSet.add(mid)
         
     return len(visitSet)
+
+
+## 이게 정해코드라고함 ~ 이거 보니까 내 코드 시갖초과 날만하다
+def solution(n):
+    count = 1
+    for j in range(1,n):
+        m1 = j
+        while True:
+            if j == n:
+                count += 1
+                break
+            elif j > n:
+                break
+            m1 += 1
+            j += m1
+
+    return count
