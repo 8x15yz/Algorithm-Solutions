299b538cc80302777135e4135579e96b9f758471
Jay <8x15yz@gmail.com>
2023-09-21 14:29:22 +0900

    Update pregrammers68936.py

--- 2023/programers/09/pregrammers68936.py
+++ 2023/programers/09/pregrammers68936.py
@@ -1,6 +1,6 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/68936
 
-## 이런 엉성한 코드가 93점짜리라니 ..
+# 아 너무 지저분한 풀이인데 암튼
 answer = []
 zero, one = 0, 0
 def solution(arr):
@@ -8,10 +8,9 @@ def solution(arr):
     l = len(arr)
     if l == 1: return 
     else:
-        quardList = [(0, l//2, 0, l//2),
-                    (0, l//2, l//2,l),
-                    (l//2,l, 0, l//2),
-                    (l//2,l, l//2,l)]
+        quardList = [(0, l//2, 0, l//2), (0, l//2, l//2,l),
+                    (l//2,l, 0, l//2), (l//2,l, l//2,l)]
+        alter = [0, 0]
         for a1, b1, a2, b2 in quardList: 
             newArr = []
             cnt = 0
@@ -24,10 +23,13 @@ def solution(arr):
 
             if cnt == (l//2)**2: 
                 one += 1
+                alter[1] += 1
                 continue
             elif cnt == 0: 
                 zero += 1
+                alter[0] += 1
                 continue
             solution(newArr)
+        if alter[0] == 4: return [1, 0]
+        elif alter[1] == 4: return [0, 1]
     return [zero, one]
-            
