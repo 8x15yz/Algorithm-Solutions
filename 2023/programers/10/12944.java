2118b2add0fe8686007ad38f16acb6f77e939bad
Jay <8x15yz@gmail.com>
2023-10-05 10:36:10 +0900

    Update 12944.java

--- 2023/programers/10/12944.java
+++ 2023/programers/10/12944.java
@@ -1,1 +1,19 @@
+// 캐스팅 연습 참고 https://mainia.tistory.com/2020
 
+public class GetMean {
+    public double getMean(int[] array) {
+      int Sum = 0;
+      for(int i = 0; i < array.length; i++){
+                 Sum = Sum + array[i];
+      }
+        return Sum / (double) array.length;
+    }
+}
+
+
+class Solution {
+    public double solution(int[] arr) {
+        GetMean getMean = new GetMean();
+        return getMean.getMean(arr);
+    }
+}
