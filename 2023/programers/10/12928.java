704c69af2ec412b9bcb3c502e00b7cbb2e08686a
Jay <8x15yz@gmail.com>
2023-10-05 13:38:49 +0900

    Update 12928.java

--- 2023/programers/10/12928.java
+++ 2023/programers/10/12928.java
@@ -1,1 +1,20 @@
+public class SumDivision {
+    public int sumDivision(int num) {
+        int answer = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num%i == 0) {
+                // System.out.println(i);
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
 
+class Solution {
+    public int solution(int n) {
+        SumDivision sumDivision = new SumDivision();
+        int answer = sumDivision.sumDivision(n);
+        return answer;
+    }
+}
