6cdb1edca2520f4aaab3303ddc5a6dae57c98167
Jay <8x15yz@gmail.com>
2023-10-05 11:27:41 +0900

    Update 12937.java

--- 2023/programers/10/12937.java
+++ 2023/programers/10/12937.java
@@ -1,1 +1,21 @@
+// 나머지연산자 참고 https://initstory.tistory.com/56
+public class GetNum {
+    public String getNum(int num) {
+        String answer = "";
+        if (num % 2 == 1) {
+            answer = "Odd";
+        }
+        else {
+            answer = "Even";
+        }
+        return answer;
+    }
+}
+
+class Solution {
+    public String solution(int num) {
+        GetNum getNum = new GetNum();
+        return getNum.getNum(num);
+    }
+}
 
