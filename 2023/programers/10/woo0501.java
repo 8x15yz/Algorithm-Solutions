084a91f3cc7deb52b167c7dbdabe85c345ec5c8b
Jay <8x15yz@gmail.com>
2023-10-10 14:54:49 +0900

    Update woo0501.java

--- 2023/programers/10/woo0501.java
+++ 2023/programers/10/woo0501.java
@@ -1,10 +1,24 @@
-package onboarding;
+// 자바에서 정수어레이 다루기 https://www.techiedelight.com/ko/convert-list-integer-array-int/
 
+package project;
 import java.util.List;
+import java.util.Arrays;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
         return answer;
     }
+}
+public class Main {
+    static Problem1 solution = new Problem1();
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello goorm!????");
+        List<Integer> pobi = Arrays.asList(97, 98); 
+        List<Integer> crong = Arrays.asList(197, 198);
+        
+        System.out.println(solution.solution(pobi, crong));
+    }
+
 }
