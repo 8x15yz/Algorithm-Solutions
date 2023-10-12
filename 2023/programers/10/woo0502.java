07984a72b744e359b99a9fa6d23d4093b43450d1
Jay <8x15yz@gmail.com>
2023-10-12 14:18:28 +0900

    Update woo0502.java

--- 2023/programers/10/woo0502.java
+++ 2023/programers/10/woo0502.java
@@ -1,10 +1,11 @@
 // 풀고잇는중 ..
-
+// 파이썬으로 갈기고 싶다 이 문제
 // input[i] == input[i-1] 이걸로는 비교할 수 없다는거 : input[i].equals(input[i-1]) 이걸로 해야 먹힘 !!!!!!!기억해 
 // String[]을 String으로 : String.join() => https://developer-talk.tistory.com/451
+
 class Problem2 {
-    static String output = "";
     public static String solution(String[] input) {
+        String output = "";
         int pointer = 0;
         int compare = 1;
         boolean isEnd = false;
@@ -23,9 +24,7 @@ class Problem2 {
             pointer = compare;
             compare += 1;
             if (compare == input.length || pointer == input.length){isEnd = true;}
-            
         }
-        // System.out.println(output); 
         return output;
     }
 }
@@ -33,19 +32,15 @@ class Problem2 {
 public class Main {
     static Problem2 solution = new Problem2();
     public static void main(String[] args) {
-        String cryptogram = "browoanoommnaon" + " ";
+        String cryptogram = "zyelleyz" + " ";
         String[] input = String.valueOf(cryptogram).split("");
         String sResult = "";
-        for (int k = 0; k < 3; k++) {     
-            System.out.println("결과");
+        while (true) {    
+            sResult = solution.solution(input)+" ";
             System.out.println(sResult);
-            System.out.println(String.join("", input));
-            sResult = "";
-            sResult = solution.solution(input);
+            if (sResult.equals(String.join("", input)) || sResult == "") break;
             input = String.valueOf(sResult).split("");
-            System.out.println(String.join("", input));
-            if (sResult.equals(input)) break;
-            
         }
+        System.out.println(sResult);
     }
 }
