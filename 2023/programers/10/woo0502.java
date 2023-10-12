63c5b1d8c7a2c73292b828e92dd87fd11e050e84
Jay <8x15yz@gmail.com>
2023-10-12 13:56:59 +0900

    Update woo0502.java

--- 2023/programers/10/woo0502.java
+++ 2023/programers/10/woo0502.java
@@ -1,22 +1,51 @@
 // 풀고잇는중 ..
+
+// input[i] == input[i-1] 이걸로는 비교할 수 없다는거 : input[i].equals(input[i-1]) 이걸로 해야 먹힘 !!!!!!!기억해 
+// String[]을 String으로 : String.join() => https://developer-talk.tistory.com/451
 class Problem2 {
-    public static int solution(String[] input) {
-        String[] answer = new String[input.length];
-        System.out.println("이거요");        
-        for (String i: input) {System.out.println(i);}
-        // for (int i = 0; i < input.length; i++) {
-        //     // answer[i] = 
-        // }
-        return 0;
+    static String output = "";
+    public static String solution(String[] input) {
+        int pointer = 0;
+        int compare = 1;
+        boolean isEnd = false;
+        boolean isSame = false;
+        
+        while (isEnd == false) {            
+            while (input[pointer].equals(input[compare]) && !isEnd) {
+                compare += 1;
+                isSame = true;
+                if (compare == input.length){isEnd = true;}
+            }
+            if (!isSame) {
+                output += input[pointer];
+            }
+            isSame = false;
+            pointer = compare;
+            compare += 1;
+            if (compare == input.length || pointer == input.length){isEnd = true;}
+            
+        }
+        // System.out.println(output); 
+        return output;
     }
 }
 
 public class Main {
     static Problem2 solution = new Problem2();
     public static void main(String[] args) {
-        String cryptogram = "browoanoommnaon";
+        String cryptogram = "browoanoommnaon" + " ";
         String[] input = String.valueOf(cryptogram).split("");
-        System.out.println(solution.solution(input));        
-        System.out.println("dkfj");
+        String sResult = "";
+        for (int k = 0; k < 3; k++) {     
+            System.out.println("결과");
+            System.out.println(sResult);
+            System.out.println(String.join("", input));
+            sResult = "";
+            sResult = solution.solution(input);
+            input = String.valueOf(sResult).split("");
+            System.out.println(String.join("", input));
+            if (sResult.equals(input)) break;
+            
+        }
     }
 }
