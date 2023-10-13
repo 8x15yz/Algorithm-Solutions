dddbd8af7b56b27c5d9b526fd2e089eb2f55a000
Jay <8x15yz@gmail.com>
2023-10-13 16:55:54 +0900

    Update woo0506.py

--- 2023/programers/10/woo0506.py
+++ 2023/programers/10/woo0506.py
@@ -1,18 +1,14 @@
 forms = [ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]
 result = set()
-
 for i in range(len(forms)-1):
     for k in range(len(forms[i][1])-1):
         pntStr = forms[i][1][k]+forms[i][1][k+1]
-        for j in range(i+1, len(forms)):
+        for j in range(i+1, len(forms)): ## 겹치면 이부분에서 brake 해야됨
             for l in range(len(forms[j][1])-1):
                 comStr = forms[j][1][l]+forms[j][1][l+1]
                 if pntStr == comStr: 
                     result.add(forms[i][0])
                     result.add(forms[j][0])
-                  
+                    break
+
 print(list(result))
-## break 문 필요함
-
-
-                
