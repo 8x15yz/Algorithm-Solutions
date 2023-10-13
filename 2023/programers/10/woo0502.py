8be680d9e39f2c0946b2b6b90df5c53790256647
Jay <8x15yz@gmail.com>
2023-10-13 11:11:46 +0900

    Update woo0502.py

--- 2023/programers/10/woo0502.py
+++ 2023/programers/10/woo0502.py
@@ -1,13 +1,26 @@
-cryptogram = "browoanoommnaon*"
-pointer, compare = 0, 1
-isSame = [0 for _ in range(len(cryptogram))]
-answer = ""
+def compareStr(cryptogram):
+    print(cryptogram)
+    pointer, compare = 0, 1
+    isSame = [0 for _ in range(len(cryptogram))]
+    answer = ""
 
-while compare < len(cryptogram):
-    while cryptogram[pointer] == cryptogram[compare]:
-        isSame[pointer], isSame[compare] = 1, 1
-        compare += 1
-    if not isSame[pointer]: answer += cryptogram[pointer]
-    pointer, compare = compare, compare+1
+    while compare < len(cryptogram):
+        while cryptogram[pointer] == cryptogram[compare]:
+            isSame[pointer], isSame[compare] = 1, 1
+            compare += 1
+        if not isSame[pointer]: answer += cryptogram[pointer]
+        pointer, compare = compare, compare+1
 
-print(answer)
+    return answer
+
+    
+    
+cry = "browoanoommnaon"
+inVal = cry+"*"
+
+while True:
+    outVal = compareStr(inVal)
+    if inVal == outVal+"*": break
+    inVal = outVal+"*"
+    
+print(outVal)
