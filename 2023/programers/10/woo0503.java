f518df3478ef578900529ec781168b8e3e86e7a0
Jay <8x15yz@gmail.com>
2023-10-13 13:09:24 +0900

    Update woo0503.java

--- 2023/programers/10/woo0503.java
+++ 2023/programers/10/woo0503.java
@@ -1,1 +1,32 @@
+// 너무쉬웟더 10분컷
 
+class Problem2 {
+    public int solution(String[] number) {
+        int outVal = 0;
+        String[] TSN = {"3", "6", "9"};
+        for(String n : number){
+            for (String t : TSN) {
+                int temp = 0;
+                if (n.equals(t)) {
+                    temp += 1;
+                }
+                outVal += temp;
+            }
+        }
+        return outVal;
+    }
+}
+
+public class Main {
+    static Problem2 solution = new Problem2();
+    public static void main(String[] args) {
+        int answer = 0;
+        int req = 33;
+        for (int i = 1; i <= req; i++) {
+            String[] num = String.valueOf(i).split("");
+            answer += solution.solution(num);
+        }
+        System.out.println(answer);
+
+    }
+}
