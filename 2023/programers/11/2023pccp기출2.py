9a2fda42a3abff0c19d14f9290c420fd9ccfa4a8
Jay <8x15yz@gmail.com>
2023-11-23 14:40:02 +0900

    Update 2023pccp기출2.py

--- 2023/programers/11/2023pccp기출2.py
+++ 2023/programers/11/2023pccp기출2.py
@@ -1,1 +1,35 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/250136
+
+v = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+def solution(land):
+    w, h = len(land[0]), len(land)
+    visit = [[0 for _ in range(w)] for _ in range(h)]
+    res = []
+    for i in range(h):
+        for j in range(w):
+            if visit[i][j] == 0:
+                visit[i][j] = 1
+                queue = [(i, j)]
+                minW, maxW = w+1, -1
+                cnt = 0
+                while queue:
+                    si, sj = queue.pop(0)
+                    for vi, vj in v:
+                        ni, nj = si+vi, sj+vj
+                        if 0 <= ni < h and 0 <= nj < w and visit[ni][nj] != 2 and land[ni][nj] == 1:
+                            queue.append((ni, nj))
+                            visit[ni][nj] = 2
+                            minW = min(minW, nj)
+                            maxW = max(maxW, nj)
+                            cnt += 1
+                if cnt != 0:
+                    res.append((cnt, minW, maxW))
+    answer = 0    
+    for i in range(w):
+        total = 0
+        for cnt, minW, maxW in res:
+            if minW<= i <=maxW:
+                total += cnt
+        answer = max(answer, total)
+                
+    return answer
