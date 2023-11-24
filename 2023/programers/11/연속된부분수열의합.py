7c0bf0c5a99fedf00cf08f8de037410da959bdf9
Jay <8x15yz@gmail.com>
2023-11-24 11:09:04 +0900

    Update 연속된부분수열의합.py

--- 2023/programers/11/연속된부분수열의합.py
+++ 2023/programers/11/연속된부분수열의합.py
@@ -10,3 +10,20 @@ def solution(sequence, k):
                     answer.append([i-j, i-1])
     print(answer)
     return answer
+
+# 고친거
+def solution(sequence, k):
+    answer = []
+    mindex = float("inf") 
+    minrge = float("inf") 
+    l = -1
+    for i in range(len(sequence)-1, 0, -1):
+        l += 1
+        total = 0
+        for j in range(0, len(sequence)-l):
+            total += sequence[i-j]
+            if total == k: 
+                if j < minrge and i-j < mindex:
+                    minrge, mindex = j, i-j
+            elif total > k: break
+    return [mindex, mindex+minrge]
