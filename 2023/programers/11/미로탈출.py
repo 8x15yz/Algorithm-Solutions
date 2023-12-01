c616927b773206a3fead7f6044f3bc7c00a02e13
Jay <8x15yz@gmail.com>
2023-12-01 10:07:42 +0900

    Update 미로탈출.py

--- 2023/programers/11/미로탈출.py
+++ 2023/programers/11/미로탈출.py
@@ -34,3 +34,36 @@ def solution(maps):
     else: i, j, visit, cnt = findE
     answer += cnt
     return answer
+
+# 12/1 정해코드
+# visit을 bfs 안쪽으로 넣어주면서 해결
+def find(maps):
+    for i in range(len(maps)):
+        for j in range(len(maps[0])):
+            if maps[i][j] == "S":
+                return i, j
+    
+d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+def bfs(maps, i, j, flag):
+    visit = [[0 for _ in range(len(maps[0]))] for _ in range(len(maps))]
+    queue = [(i, j, 1)]
+    while queue:
+        si, sj, cnt = queue.pop(0)
+        if maps[si][sj] == flag:
+            return si, sj, visit, cnt
+        for di, dj in d:
+            ni, nj = si+di, sj+dj
+            if 0 <= ni < len(maps) and 0 <= nj < len(maps[0]) and visit[ni][nj] == 0 and maps[ni][nj] != "X":
+                queue.append((ni, nj, cnt+1))
+                visit[ni][nj] = cnt
+    return -1
+
+def solution(maps):
+    answer = -2
+    i, j = find(maps)
+    for target in ["L", "E"]:
+        finds = bfs(maps, i, j, target)
+        if finds == -1: return -1
+        else: i, j, visit, cnt = finds
+        answer += cnt
+    return answer
